8b9eeaa1f3f79b425a6e817626ff4516422bd2ac
Mikhael Goikhman <migo@freeshell.org>
2014-06-24 18:40:24 +0300

    refactor and set the whole NBSS packet in parser rather than SMB-only

--- lib/SMB/Connection.pm
+++ lib/SMB/Connection.pm
@@ -13,12 +13,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+package SMB::Connection;
+
 use strict;
 use warnings;
 
 use bytes;
-
-package SMB::Connection;
 
 use parent 'SMB';
 
@@ -47,35 +47,54 @@ sub new ($$;$%) {
 sub DESTROY ($) {
 	my $self = shift;
 
-	$self->msg("Disconnected");
-
-	$self->socket->close;
+	$self->close;
 }
 
-sub recv_command ($) {
+sub close ($) {
 	my $self = shift;
 
 	my $socket = $self->socket;
-	my ($len, $data);
+	return unless $socket && $socket->opened;
+
+	$self->msg("Disconnected");
+
+	$socket->close;
+	$self->socket(undef);
+}
+
+sub recv_nbss ($) {
+	my $self = shift;
+
+	my $socket = $self->socket;
+	my $data1;  # NBSS header
+	my $data2;  # SMB packet
 	my $header_label = 'NetBIOS Session Service header';
-	$len = $socket->read($data, 4) //
+	my $len = $socket->read($data1, 4) //
 		return $self->err("Read failed: $!");
 	if ($len != 4) {
 		$self->err("Can't read $header_label (got $len bytes)");
 		return;
 	}
-	my ($packet_type, $packet_flags, $packet_len) = unpack('CCn', $data);
+	my ($packet_type, $packet_flags, $packet_len) = unpack('CCn', $data1);
 	if ($packet_type != 0 || $packet_flags > 1) {
 		$self->err("Only supported $header_label with type=0 flags=0|1");
 		return;
 	}
 	$packet_len += 1 << 16 if $packet_flags;
-	$len = $socket->read($data, $packet_len) // 0;
+	$len = $socket->read($data2, $packet_len) // 0;
 	if ($len != $packet_len) {
 		$self->err("Can't read full packet (expected $packet_len, got $len bytes)");
 		return;
 	}
-	$self->parser->set($data);
+
+	$self->parser->set($data1 . $data2, 4);
+}
+
+sub recv_command ($) {
+	my $self = shift;
+
+	$self->recv_nbss
+		or return;
 
 	my $smb_num = $self->parse_uint8;
 	my $smb_str = $self->parse_bytes(3);
@@ -85,7 +104,7 @@ sub recv_command ($) {
 		return;
 	}
 	my $is_smb1 = $smb_num == 0xff;
-	$self->mem($data, "<- SMB Packet");
+	$self->mem($self->parser->data, "<- SMB Packet");
 
 	my $command = $is_smb1
 		? $self->parse_smb1
@@ -100,6 +119,17 @@ sub recv_command ($) {
 	return $command;
 }
 
+sub send_nbss ($$) {
+	my $self = shift;
+	my $data = shift;
+
+	$self->mem($data, "-> NetBIOS Packet");
+	if (!$self->socket->write($data, length($data))) {
+		$self->err("Can't write full packet");
+		return;
+	}
+}
+
 sub send_command ($$) {
 	my $self = shift;
 	my $command = shift;
@@ -112,14 +142,7 @@ sub send_command ($$) {
 		? $self->pack_smb1($command, is_response => 1)
 		: $self->pack_smb2($command, is_response => 1);
 
-	my $data = $self->packer->data;
-	my $size = $self->packer->size;
-	$self->mem($data, "-> NetBIOS Packet");
-
-	if (!$self->socket->write($data, $size)) {
-		$self->err("Can't write full packet");
-		return;
-	}
+	$self->send_nbss($self->packer->data);
 }
 
 sub log ($$$) {
